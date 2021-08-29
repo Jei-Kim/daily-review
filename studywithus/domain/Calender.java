6c2ccfe70b009834b5c7cae63d182cb2b4b76bc0
jei-kim <jeje2kim@gmail.com>
2021-08-29 23:51:46 +0900

    .

---
+++ studywithus/domain/Calender.java
@@ -0,0 +1,18 @@
+package com.studywithus.domain;
+import java.sql.Date;
+
+
+
+public class Calender {
+
+
+  public int no;
+  public String title;
+  public String content;
+  public Date startDate;
+  public Date endDate;
+  public String memo;
+
+
+}
+
---
+++ studywithus/domain/NewMember.java
@@ -0,0 +1,9 @@
+package com.studywithus.domain;
+
+public class NewMember {
+  public static String adminId = "admin";
+  public static String adminPwd = "admin";
+  public String name;
+  public String id;
+  public String pwd;
+}
