b3c87ce2f991a981299d6ef002f25d601c99d447
Jei-Kim <jeje2kim@gmail.com>
2021-09-03 20:03:54 +0900

    .

--- week10/day49-0903/app-14-f  to 15/src/main/java/com/eomcs/pms/handler/ProjectPrompt.java
+++ week10/day49-0903/app-14-f  to 15/src/main/java/com/eomcs/pms/handler/ProjectPrompt.java
@@ -1,36 +1,15 @@
 package com.eomcs.pms.handler;
 
 import java.util.List;
-import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Project;
 import com.eomcs.util.Prompt;
 
-public abstract class AbstractProjectHandler {
+public class ProjectPrompt {
 
   protected List<Project> projectList;
 
-  public AbstractProjectHandler(List<Project> projectList) {
+  public ProjectPrompt(List<Project> projectList) {
     this.projectList = projectList;
-  }
-
-  protected String getMemberNames(List<Member> members) {
-    StringBuilder names = new StringBuilder();
-    for (Member member : members) {
-      if (names.length() > 0) {
-        names.append(",");
-      }
-      names.append(member.getName());
-    }
-    return names.toString();
-  }
-
-  protected Project findByNo(int no) {
-    for (Project project : projectList) {
-      if (project.getNo() == no) {
-        return project;
-      }
-    }
-    return null;
   }
 
   public Project promptProject() {
@@ -51,9 +30,12 @@ public abstract class AbstractProjectHandler {
     }
   }
 
+  protected Project findByNo(int no) {
+    for (Project project : projectList) {
+      if (project.getNo() == no) {
+        return project;
+      }
+    }
+    return null;
+  }
 }
-
-
-
-
-
