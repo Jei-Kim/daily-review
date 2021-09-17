4e78a6f236f3c6f201522dd2f2b6ec1449a094a8
Jei-Kim <jeje2kim@gmail.com>
2021-09-17 14:49:50 +0900

    .

---
+++ week12/day59-0917/app-16-b/src/main/java/com/eomcs/pms/handler/BoardDetailHandler.java
@@ -0,0 +1,71 @@
+package com.eomcs.pms.handler;
+
+import java.util.List;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
+
+public class BoardDetailHandler extends AbstractBoardHandler {
+
+  public BoardDetailHandler(
+    List<Board> boardList, 
+    BoardUpdateHandler boardUpdateHandler) {
+    super(boardList);
+   
+  }
+
+  @Override
+  public void execute(CommandRequest request) throws Exception {
+    System.out.println("[게시글 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = findByNo(no);
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    System.out.printf("제목: %s\n", board.getTitle());
+    System.out.printf("내용: %s\n", board.getContent());
+    System.out.printf("작성자: %s\n", board.getWriter().getName());
+    System.out.printf("등록일: %s\n", board.getRegisteredDate());
+
+    board.setViewCount(board.getViewCount() + 1);
+    System.out.printf("조회수: %d\n", board.getViewCount());
+    System.out.println();
+
+    // 추가
+    Member loginUser = AuthLoginHandler.getLoginUser();
+    if (loginUser == null || board.getWriter.getNo() != loginUser.getNo()) {
+      return;
+    }
+// BoardUpdateHandler나 BoardDeleteHandler를 실행할 때
+// 
+    request.setAttribute("no", no);
+
+    while (true){
+    String input = Prompt.inputString("변경(U), 삭제(D), 이전(0)>");
+    switch (input) {
+      case "U":
+      case "u":
+        request.getRequestDispatcher("/board/update").forward(request);
+        return;
+      case "D":
+      case "d":
+        boardDeleteHandler.execute("/board/delete").forward(request);;
+        return;
+      case "0":
+      //이전 메뉴로 나가기
+      return;
+      default:
+      System.out.println("명령어가 올바르지 않습니다.")
+      
+      }
+    }
+  }
+}
+
+
+
+
+
